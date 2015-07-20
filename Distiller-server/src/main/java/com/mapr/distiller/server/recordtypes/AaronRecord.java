13338cbe84f4660771e1e9594a0ed7b559cf5570
aaroneng <aeng@maprtech.com>
2015-07-20 13:54:06 -0400

    abc

--- Distiller-server/src/main/java/com/mapr/distiller/server/recordtypes/AaronRecord.java
+++ Distiller-server/src/main/java/com/mapr/distiller/server/recordtypes/AaronRecord.java
@@ -1,1 +1,28 @@
+package com.mapr.distiller.server.recordtypes;
+
+public class SplitterRecord extends Record {
+	private String type;
+	private Record record;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Record getRecord() {
+		return record;
+	}
+
+	public void setRecord(Record record) {
+		this.record = record;
+	}
+
+	@Override
+	public String toString() {
+		return record.toString();
+	}
+}
 somenewfile
